f6379eb435f67597c4fabd08651d89ac442fad69
Rotsen Mark Acob <acob.rotsen@hotmail.com>
2017-06-27 13:32:13 +0800

    Replaced css-nano with clean-css

--- gulpfile.js
+++ gulpfile.js
@@ -1,41 +1,37 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
-    autoprefixer = require('autoprefixer'),
-    postcss = require('gulp-postcss'),
-    cssnano = require('cssnano'),
+    autoprefixer = require('gulp-autoprefixer'),
+    cleancss = require('gulp-clean-css'),
     jshint = require('gulp-jshint'),
     uglify = require('gulp-uglify'),
     rename = require('gulp-rename'),
     concat = require('gulp-concat'),
     notify = require('gulp-notify'),
     cache = require('gulp-cache'),
-    prettify = require('gulp-jsbeautifier'),
     vinylpaths = require('vinyl-paths'),
     cmq = require('gulp-combine-mq'),
     merge = require('merge-stream'),
     foreach = require('gulp-flatmap'),
     changed = require('gulp-changed'),
     runSequence = require('run-sequence'),
+    wait = require('gulp-wait'),
     del = require('del');
 
 // CSS
 gulp.task('styles', function(){
-    var plugins = [
-        autoprefixer({browsers: ['last 1 version']}),
-        cssnano()
-    ];
 
     var sassStream = gulp.src('assets/scss/style.scss')
+        .pipe(wait(400))
         .pipe(sass.sync().on('error', sass.logError))
         .pipe(concat('app.scss'))
     
     var mergeStream = merge(sassStream)
         .pipe(concat('app.css'))
-        .pipe(postcss(plugins))
+        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
         .pipe(cmq())
         .pipe(gulp.dest('temp/css'))
         .pipe(rename('app.css'))
-        .pipe(prettify())
+        .pipe(cleancss())
         .pipe(gulp.dest('assets/css'))
         .pipe(notify({ message: 'Styles task complete' }));
     
@@ -69,14 +65,6 @@ gulp.task('scripts', function() {
 gulp.task('clean', function(cb) {
     return gulp.src('temp/*')
     .pipe(vinylpaths(del))
-});
-
-// Copy bootstrap fonts to assets folder
-gulp.task('fonts', function() {
-    return gulp.src(['bower_components/bootstrap-sass/assets/fonts/bootstrap/**/*'], {
-        base: 'bower_components/bootstrap-sass/assets/fonts'
-    })
-    .pipe(gulp.dest('assets/fonts'));
 });
 
 // Default task
