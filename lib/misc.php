5dd99c75641ce9ef70e16f27e312e832480394b4
Rotsen Mark Acob <acob.rotsen@hotmail.com>
2017-12-08 10:34:26 +0800

    Removed unneeded line of codes

--- lib/misc.php
+++ lib/misc.php
@@ -47,57 +47,6 @@ function gb_execute_php( $html ) {
 	return $html;
 }
 
-//* Theme Settings
-add_action( 'get_header', 'gb_do_theme_settings' );
-function gb_do_theme_settings() {
-	if ( get_theme_mod( 'logo', false ) ) {
-		remove_action( 'genesis_header', 'genesis_do_header' );
-		add_action( 'genesis_header', 'gb_do_header' );
-	}
-}
-
-//* Custom Header
-function gb_do_header() {
-    global $wp_registered_sidebars;
-
-    genesis_markup( array(
-        'html5'   => '<div %s>',
-        'xhtml'   => '<div class="title-area" id="title-area">',
-        'context' => 'title-area'
-    ) );
-    if ( get_theme_mod( 'logo', false ) ) {
-        echo '<h1 class="site-title" itemprop="headline">';
-            echo '<a href="'.get_bloginfo( 'url' ).'" class="logo" title="'.get_bloginfo( 'name' ).'">';
-                echo '<img src="'.get_theme_mod( 'logo' ).'" alt="'.get_bloginfo( 'name' ).'"/>';
-            echo '</a>';
-        echo '</h1>';
-    } else {
-        do_action( 'genesis_site_title' );
-        do_action( 'genesis_site_description' );
-    }
-    echo '</div>';
-
-    if ( ( isset( $wp_registered_sidebars['header-right'] ) && is_active_sidebar( 'header-right' ) ) || has_action( 'genesis_header_right' ) ) {
-        genesis_markup( array(
-            'html5'   => '<aside %s>',
-            'xhtml'   => '<div class="widget-area header-widget-area">',
-            'context' => 'header-widget-area',
-        ) );
-
-            do_action( 'genesis_header_right' );
-            add_filter( 'wp_nav_menu_args', 'genesis_header_menu_args' );
-            add_filter( 'wp_nav_menu', 'genesis_header_menu_wrap' );
-            dynamic_sidebar( 'header-right' );
-            remove_filter( 'wp_nav_menu_args', 'genesis_header_menu_args' );
-            remove_filter( 'wp_nav_menu', 'genesis_header_menu_wrap' );
-
-        genesis_markup( array(
-            'html5' => '</aside>',
-            'xhtml' => '</div>',
-        ) );
-    }
-}
-
 //* This will occur when the comment is posted
 function gb_comment_post( $incoming_comment ) {
     // convert everything in a comment to display literally
