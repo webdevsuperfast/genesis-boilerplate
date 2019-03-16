e51030b69a8c2780519571923a627064fba6172b
webdevsuperfast <acob.rotsen@hotmail.com>
2019-03-16 16:13:58 +0800

    Added landing page template

---
+++ templates/page_landing.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Template Name: Landing
+ *
+ * @package      Genesis Boilerplate
+ * @since        0.0.1
+ * @link         https://webdevsuperfast.github.io
+ * @author       Rotsen Mark Acob <rotsenacob.com>
+ * @copyright    Copyright (c) 2016, Rotsen Mark Acob
+ * @license      http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ *
+*/
+
+// Remove the secondary navigation
+remove_action( 'genesis_before_header', 'genesis_do_subnav' );
+
+// Remove the primary navigation
+remove_action( 'genesis_after_header', 'genesis_do_nav' ); 
+
+// Remove header
+remove_action( 'genesis_header', 'genesis_do_header' );
+remove_action( 'genesis_header', 'genesis_header_markup_open', 5 );
+remove_action( 'genesis_header', 'genesis_header_markup_close', 15 );
+
+// Remove Footer Widgets
+remove_action( 'genesis_before_footer', 'genesis_footer_widget_areas' );
+
+// Remove Footer
+remove_action( 'genesis_footer', 'genesis_do_footer' );
+remove_action( 'genesis_footer', 'genesis_footer_markup_open', 5 );
+remove_action( 'genesis_foote', 'genesis_footer_markup_close', 15 );
+
+// Force full-width-content layout setting
+add_filter( 'genesis_pre_get_option_site_layout', '__genesis_return_full_width_content' );
+
+// Remove Breadcrumbs
+remove_action( 'genesis_before_loop', 'genesis_do_breadcrumbs' );
+
+// Remove Skip Links from a template
+remove_action ( 'genesis_before_header', 'genesis_skip_links', 5 );
+
+// Dequeue Skip Links Script
+add_action( 'wp_enqueue_scripts','child_dequeue_skip_links' );
+function child_dequeue_skip_links() {
+	wp_dequeue_script( 'skip-links' );
+}
+
+genesis();
